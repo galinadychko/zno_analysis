527eb02b3bd6183ebfefab9212e5084006996b8f
galinadychko <galiadychko@outlook.com>
2019-02-07 12:04:22 +0200

    feat: Add spliting data for k-folds crossvalidation

--- tools/DataSplitTools.R
+++ tools/DataSplitTools.R
@@ -5,3 +5,13 @@ train_test_split <- function(df, ratio=0.80){
   id <- sample(nrows, nsample, replace = FALSE)
   return(list(train = df[id, ], test = df[!(1:nrows %in% id), ]))
 }
+
+cross_validation_split <- function(df, k=5){
+  nrows <- nrow(df)
+  df2 <- df[sample(nrows, nrows, replace = FALSE),]
+  
+  sample_size <- ceiling(nrows/k)
+  id_list <- split(1:(k*sample_size), rep(1:k, each = sample_size))
+  id_list[[k]] <- id_list[[k]][id_list[[k]] <= nrows]
+  return(lapply(id_list, function(x){df2[x, ]}))
+}
