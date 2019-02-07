819ec05e47185be635e806d43930758bc42c0dff
galinadychko <galiadychko@outlook.com>
2019-02-07 12:06:25 +0200

    feat: Add k-folds crossvalidation

--- tools/DataSplitTools.R
+++ tools/DataSplitTools.R
@@ -1,3 +1,5 @@
+source("tools/GeneralizedNadarayaWatson.R")
+
 
 train_test_split <- function(df, ratio=0.80){
   nrows <- nrow(df)
@@ -14,4 +16,18 @@ cross_validation_split <- function(df, k=5){
   id_list <- split(1:(k*sample_size), rep(1:k, each = sample_size))
   id_list[[k]] <- id_list[[k]][id_list[[k]] <= nrows]
   return(lapply(id_list, function(x){df2[x, ]}))
+}
+
+cross_validation <- function(cv_split, func, parameters){
+  k <- length(cv_split)
+  cv_names <- names(cv_split)
+  res <- lapply(1:k, 
+                function(x){
+                  test <- cv_split[[x]]
+                  train <- do.call(rbind, cv_split[!x %in% cv_names])
+                  Y_res <- lapply(test["x"], 
+                                  function(x){
+                                    return(nadaraya_watson(x, train["x"], train["y"], parameters["h"], parameters["A"]))
+                                  })
+                })
 }
