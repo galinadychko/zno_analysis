a1deffb0e86a84a0f107beb7996d524b08bd94ba
galinadychko <galiadychko@outlook.com>
2019-02-07 17:03:27 +0200

    fix: Use more general exmples`s values

--- tests/test_MVCweights.R
+++ tests/test_MVCweights.R
@@ -3,10 +3,10 @@ source("tools/TestsTools.R")
 
 
 test_that("Test Gramm_matrix(not_matrix)",{
-  expect_error(Gramm_matrix(c(0, 1)), "Not appropriate input format")
-  expect_error(Gramm_matrix(data.frame(0, 1)), "Not appropriate input format")
-  expect_error(Gramm_matrix(list(0, 1)), "Not appropriate input format")
-  expect_error(Gramm_matrix(factor(0, 1)), "Not appropriate input format")
+  expect_error(Gramm_matrix(c(NA, NA)), "Not appropriate input format")
+  expect_error(Gramm_matrix(data.frame(NA, NA)), "Not appropriate input format")
+  expect_error(Gramm_matrix(list(NA, NA)), "Not appropriate input format")
+  expect_error(Gramm_matrix(factor(NA, NA)), "Not appropriate input format")
   expect_error(Gramm_matrix("String"), "Not appropriate input format")
 })
 
@@ -36,15 +36,15 @@ test_that("Test Gramm_matrix(three_component_mixture)",{
 })
 
 test_that("Test minor(not_matrix)",{
-  expect_error(minor(c(0, 1), 1, 1), "Not appropriate input format")
-  expect_error(minor(data.frame(0, 1), 1, 1), "Not appropriate input format")
-  expect_error(minor(list(0, 1), 1, 1), "Not appropriate input format")
-  expect_error(minor(factor(0, 1), 1, 1), "Not appropriate input format")
+  expect_error(minor(c(NA, NA), 1, 1), "Not appropriate input format")
+  expect_error(minor(data.frame(NA, NA), 1, 1), "Not appropriate input format")
+  expect_error(minor(list(NA, NA), 1, 1), "Not appropriate input format")
+  expect_error(minor(factor(NA, NA), 1, 1), "Not appropriate input format")
   expect_error(minor("String", 1, 1), "Not appropriate input format")
 })
 
 test_that("Test minor(matrix, not_correct_i_j)",{
-  M <- cbind(c(0, 1), c(1, 1))
+  M <- matrix(NA, nrow = 2, ncol = 2)
   expect_error(minor(M, 0, 0), "Not correct i,j")
   expect_error(minor(M, 1, 0), "Not correct i,j")
   expect_error(minor(M, 0, 1), "Not correct i,j")
@@ -53,4 +53,12 @@ test_that("Test minor(matrix, not_correct_i_j)",{
   expect_error(minor(M, -1, -1), "Not correct i,j")
   expect_error(minor(M, -1, 1), "Not correct i,j")
   expect_error(minor(M, 1, -1), "Not correct i,j")
+})
+
+test_that("Test all_matrix_minor(not_matrix)",{
+  expect_error(all_matrix_minors(c(NA, NA)), "Not appropriate input format")
+  expect_error(all_matrix_minors(data.frame(NA, NA)), "Not appropriate input format")
+  expect_error(all_matrix_minors(list(NA, NA)), "Not appropriate input format")
+  expect_error(all_matrix_minors(factor(NA, NA)), "Not appropriate input format")
+  expect_error(all_matrix_minors("String"), "Not appropriate input format")
 })
