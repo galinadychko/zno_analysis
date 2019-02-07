68f0cdbb1fdc4bf7c47a8de26c9d2c14ffc69104
galinadychko <galiadychko@outlook.com>
2019-02-07 12:03:31 +0200

    feat: Add spliting data into train-test frames

--- tools/DataSplitTools.R
+++ tools/DataSplitTools.R
@@ -1,1 +1,7 @@
 
+train_test_split <- function(df, ratio=0.80){
+  nrows <- nrow(df)
+  nsample <- round(nrows * ratio)
+  id <- sample(nrows, nsample, replace = FALSE)
+  return(list(train = df[id, ], test = df[!(1:nrows %in% id), ]))
+}
