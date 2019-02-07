5ab4a3cfddcf77db7220c7293de51a0b0e4136d2
galinadychko <galiadychko@outlook.com>
2019-02-07 12:15:05 +0200

    feat: Add computing all matrix`s minors

--- tools/MVCweights.R
+++ tools/MVCweights.R
@@ -7,3 +7,26 @@ GramN_new <- function(weights_matrix){
   prod_ <- t(weights_matrix) %*% weights_matrix
   return((prod_)/nrows)
 }
+
+
+# -------------------
+# a - weighted coeff
+# -------------------
+
+minor <- function(A, i, j){
+  return(as.matrix(A[-i, -j]))
+}
+
+
+all_minors <- function(matrix_){
+  nrows <- nrow(matrix_)
+  ncols <- ncol(matrix_)
+  m <- matrix(nrow = ncols, ncol = ncols)
+  for (i in 1:nrows) {
+    for (j in 1:ncols) {
+      print(minor(matrix_, i, j))
+      m[i, j] <- det(minor(matrix_, i, j))
+    }
+  }
+  return(m)
+}
