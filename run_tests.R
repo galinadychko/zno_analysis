250b6c563e347b6407daf59fac876c08d3ad9cdf
galinadychko <galiadychko@outlook.com>
2019-02-08 18:49:16 +0200

    test: Add tests for DataSplit module

--- run_tests.R
+++ run_tests.R
@@ -3,5 +3,7 @@ library(testthat)
 source("tools/MVCweights.R")
 source("tools/NadarayaWatson.R")
 source("tools/GeneralizedNadarayaWatson.R")
+source("tools/DataSplitTools.R")
 
 test_results <- test_dir("tests", reporter = "summary")
+
