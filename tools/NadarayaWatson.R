85fba16fd453d5e53af792d3c64b8307d6d6d1a7
galinadychko <galiadychko@outlook.com>
2019-02-08 12:16:01 +0200

    fix: Fix nw_onw_component to function, which could compute estimation for each component

--- tools/NadarayaWatson.R
+++ tools/NadarayaWatson.R
@@ -14,22 +14,26 @@ Epanechnikov <- function(x){
 # Nadaraya-Watson for component
 # --------------------------------------------------------
 
-nw_one_component <- function(x, X_train, Y_train, h, A){
-  X_train <- as.numeric(X_train)
-  Y_train <- as.numeric(Y_train)
-  A <- as.numeric(A)
+nw_any_components <- function(x, X_train, Y_train, h, A){
+  if (is.numeric(X_train) != TRUE | is.numeric(Y_train) != TRUE | 
+      is.numeric(x) != TRUE) {stop("Not correct input type")}
+  if (is.numeric(A) == TRUE) {A <- as.matrix(A, ncol = 1)}
+  n_colms <- ncol(A)
+  numerator <- colSums(matrix(A * Y_train * Epanechnikov((x - X_train)/h), ncol = n_colms))
+  denominator <- colSums(matrix(A * Epanechnikov((x - X_train)/h), ncol = n_colms))
   
-  numerator <- sum(A * Y_train * Epanechnikov((x - X_train)/h))
-  denominator <- sum(A * Epanechnikov((x - X_train)/h))
+  if (sum(denominator) == 0) {stop("Devision by zero")}
+  
   return(numerator/denominator)
 }
 
-
-nw_all_components <- function(x, X_train, Y_train, h, A){
-  M <- ncol(A)
-  result <- sapply(1:M, 
-                   function(i, x, X_train, Y_train, h, A){
-                     return(nw_one_component(x, X_train, Y_train, h, A[, i]))
-                   }, x = x, X_train = X_train, Y_train = Y_train, h = h, A = A )
-  return(result)
-}
+# 
+# nw_all_components <- function(x, X_train, Y_train, h, A){
+#   if (is.matrix(A) != TRUE) {stop("Not correct input type")}
+#   M <- ncol(A)
+#   result <- sapply(1:M, 
+#                    function(i, x, X_train, Y_train, h, A){
+#                      return(nw_one_component(x, X_train, Y_train, h, A[, i]))
+#                    }, x = x, X_train = X_train, Y_train = Y_train, h = h, A = A)
+#   return(result)
+# }
