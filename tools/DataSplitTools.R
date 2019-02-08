4654972155f1ce301f4e333dbc94bb63d25dd7b0
galinadychko <galiadychko@outlook.com>
2019-02-08 18:50:13 +0200

    fix: Add checking statement of input data

--- tools/DataSplitTools.R
+++ tools/DataSplitTools.R
@@ -2,21 +2,27 @@ source("tools/GeneralizedNadarayaWatson.R")
 
 
 train_test_split <- function(df, ratio=0.80){
+  if (!is.matrix(df) & !is.data.frame(df) & !is.table(df)) {stop("Not correct input type")}
+  if (nrow(df) < 2) {stop("Not correct input dimension")}
   nrows <- nrow(df)
   nsample <- round(nrows * ratio)
   id <- sample(nrows, nsample, replace = FALSE)
   return(list(train = df[id, ], test = df[!(1:nrows %in% id), ]))
 }
 
+
 cross_validation_split <- function(df, k=5){
+  if (!is.matrix(df) & !is.data.frame(df) & !is.table(df)) {stop("Not correct input type")}
+  if (nrow(df) < k) {stop("Not correct input dimension")}
   nrows <- nrow(df)
-  df2 <- df[sample(nrows, nrows, replace = FALSE),]
+  df2 <- as.matrix(df[sample(nrows, nrows, replace = FALSE),], ncol = ncol(df))
   
   sample_size <- ceiling(nrows/k)
   id_list <- split(1:(k*sample_size), rep(1:k, each = sample_size))
   id_list[[k]] <- id_list[[k]][id_list[[k]] <= nrows]
   return(lapply(id_list, function(x){df2[x, ]}))
 }
+
 
 cross_validation <- function(cv_split, func, parameters){
   k <- length(cv_split)
