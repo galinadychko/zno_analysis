0d6906d32f6a800d133cd6f22e3dc3b65c89997e
galinadychko <galiadychko@outlook.com>
2019-02-11 13:50:37 +0200

    fix: Add checking correctness of input variables

--- tools/CommonTools.R
+++ tools/CommonTools.R
@@ -1,7 +1,8 @@
-"%&%" <- function(x, y){paste(x, y, sep="")}
+"%&%" <- function(x, y){paste(x, y, sep = "")}
 
 
 split_k_parts <- function(k, nrows){
+  if (nrows < k) {stop("Number of observations is smaller than number of folds")}
   sample_size <- ceiling(nrows/k)
   id_list <- split(1:(k*sample_size), rep(1:k, each = sample_size))
   id_list[[k]] <- id_list[[k]][id_list[[k]] <= nrows]
