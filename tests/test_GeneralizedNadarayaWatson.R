a32259efe7c6d65c2e1629e372fd60c043f7f15f
galinadychko <galiadychko@outlook.com>
2019-02-13 18:10:17 +0200

    fix: Add returning of acoeff for test data

--- tests/test_GeneralizedNadarayaWatson.R
+++ tests/test_GeneralizedNadarayaWatson.R
@@ -15,10 +15,10 @@ test_that("Test GeneralisedNadarayaWatson/train(not_correct_input)", {
   Y_train <- cbind(NaN)
   w_coeff <- c(NaN)
   instance <- GeneralisedNadarayaWatson$new()
-  expect_error(instance$train(X_train, Y_train, w_coeff), "Not correct class atributes")
-  expect_error(instance$train(as.numeric(X_train), Y_train, w_coeff), "Not correct class atributes")
-  expect_error(instance$train(X_train, as.numeric(Y_train), w_coeff), "Not correct class atributes")
-  expect_error(instance$train(X_train, Y_train, as.matrix(w_coeff)), "Not correct class atributes")
+  expect_error(instance$train(X_train, Y_train, w_coeff), "Not correct class attributes")
+  expect_error(instance$train(as.numeric(X_train), Y_train, w_coeff), "Not correct class attributes")
+  expect_error(instance$train(X_train, as.numeric(Y_train), w_coeff), "Not correct class attributes")
+  expect_error(instance$train(X_train, Y_train, as.matrix(w_coeff)), "Not correct class attributes")
   expect_error(instance$train(rep(NaN, 2), rep(NaN, 2), matrix(NaN, 1, 2)), "Not correct input dimensions")
   expect_error(instance$train(rep(NaN, 1), rep(NaN, 2), matrix(NaN, 2, 2)), "Not correct input dimensions")
   expect_error(instance$train(rep(NaN, 1), rep(NaN, 1), matrix(NaN, 2, 2)), "Not correct input dimensions")
@@ -29,21 +29,21 @@ test_that("Test GeneralisedNadarayaWatson/train(correct_input)", {
   X_train <- rep(NaN, 2)
   Y_train <- rep(NaN, 2)
   w_coeff <- wcoeff_two_components(n = 2)
-  w_coeff2 <- rbind(c(0.8, 0.1, 0.1), 
-                    c(0.05, 0.90, 0.05), 
+  w_coeff2 <- rbind(c(0.8, 0.1, 0.1),
+                    c(0.05, 0.90, 0.05),
                     c(0.2, 0.1, 0.7))
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(X_train, Y_train, w_coeff)
   expect_equal(instance$X_train, rep(NaN, 2))
   expect_equal(instance$Y_train, rep(NaN, 2))
   expect_equal(instance$A, cbind(c(0, 2), c(4, -2)))
-  
+
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(rep(NaN, 3), rep(NaN, 3), w_coeff2)
   expect_equal(instance$X_train, rep(NaN, 3))
   expect_equal(instance$Y_train, rep(NaN, 3))
-  expect_equal(instance$A, cbind(c(3.90625, -0.37500, -0.53125), 
-                                 c(-0.15625, 3.37500, -0.21875), 
+  expect_equal(instance$A, cbind(c(3.90625, -0.37500, -0.53125),
+                                 c(-0.15625, 3.37500, -0.21875),
                                  c(-1.09375, -0.37500, 4.46875)))
 })
 
@@ -52,8 +52,8 @@ test_that("Test types GeneralisedNadarayaWatson/train(correct_input)", {
   X_train <- rep(NaN, 2)
   Y_train <- rep(NaN, 2)
   w_coeff <- wcoeff_two_components(n = 2)
-  w_coeff2 <- rbind(c(0.8, 0.1, 0.1), 
-                    c(0.05, 0.90, 0.05), 
+  w_coeff2 <- rbind(c(0.8, 0.1, 0.1),
+                    c(0.05, 0.90, 0.05),
                     c(0.2, 0.1, 0.7))
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(X_train, Y_train, w_coeff)
@@ -65,12 +65,14 @@ test_that("Test types GeneralisedNadarayaWatson/train(correct_input)", {
 
 test_that("Test GeneralisedNadarayaWatson/predict(not_correct_input)", {
   instance <- GeneralisedNadarayaWatson$new()
-  expect_error(instance$predict(rep(NaN), 1), "The model was not trained correctly")
+  expect_error(instance$predict(rep(NaN), rbind(NaN), 1), "The model was not trained correctly")
   instance <- GeneralisedNadarayaWatson$new()
   instance$X_train <- 1:2
   instance$Y_train <- 1:2
   instance$A <- matrix(NaN, 2, 2)
-  expect_error(instance$predict(rep(NaN, 1), 1), "The model coefficients are not numbers")
+  expect_error(instance$predict(rep(NaN, 1), rbind(NaN), 1), "The model coefficients are not numbers")
+  instance$A <- matrix(1, 2, 2)
+  expect_error(instance$predict(rep(NaN, 1), matrix(NaN, 2, 2), 1), "Not correct input dimensions")
 })
 
 
@@ -85,11 +87,19 @@ test_that("Test GeneralisedNadarayaWatson/predict(correct_input)", {
                     c(0.2, 0.1, 0.7))
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(X_train, Y_train, w_coeff)
-  expect_equal(instance$predict(X_test, 1), rbind(c(2, 2), c(2, 2)))
-  expect_equal(instance$predict(X_test, .5), rbind(c(2, 2), c(2, 2)))
+  expect_equal(instance$predict(X_test, w_coeff, 1),
+               list("prediction" = rbind(c(2, 2), c(2, 2)),
+                    "A_test" = cbind(c(0, 2), c(4, -2))))
+  expect_equal(instance$predict(X_test, w_coeff, .5),
+               list("prediction" = rbind(c(2, 2), c(2, 2)),
+                    "A_test" = cbind(c(0, 2), c(4, -2))))
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(c(X_train, 3), c(Y_train, 1), w_coeff2)
-  expect_equal(instance$predict(X_test2, 1), rbind(c(2, 2, 2), c(2, 2, 2)))
+  expect_equal(instance$predict(c(X_test2, 1), w_coeff2, 1),
+               list("prediction" = rbind(c(2, 2, 2), c(2, 2, 2), c(2, 2, 2)),
+                    "A_test" = cbind(c(3.90625, -0.37500, -0.53125),
+                                     c(-0.15625, 3.37500, -0.21875),
+                                     c(-1.09375, -0.37500, 4.46875))))
 })
 
 
@@ -104,30 +114,44 @@ test_that("Test output type GeneralisedNadarayaWatson/predict", {
                     c(0.2, 0.1, 0.7))
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(X_train, Y_train, w_coeff)
-  expect_true(is.matrix(instance$predict(X_test, 1)))
-  expect_true(is.matrix(instance$predict(X_test, .5)))
+  expect_true(is.list(instance$predict(X_test, w_coeff, 1)))
+  expect_true(is.matrix(instance$predict(X_test, w_coeff, 1)[[1]]))
+  expect_true(is.matrix(instance$predict(X_test, w_coeff, 1)[[2]]))
+  expect_true(is.matrix(instance$predict(X_test, w_coeff, 0.5)[[1]]))
+  expect_true(is.matrix(instance$predict(X_test, w_coeff, 0.5)[[2]]))
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(c(X_train, 3), c(Y_train, 1), w_coeff2)
-  expect_true(is.matrix(instance$predict(X_test2, 1)))
+  expect_true(is.list(instance$predict(X_test2, w_coeff, 1)))
+  expect_true(is.matrix(instance$predict(c(X_test2, 1), w_coeff2, 1)[[1]]))
+  expect_true(is.matrix(instance$predict(c(X_test2, 1), w_coeff2, 1)[[2]]))
 })
 
 
 test_that("Test GeneralisedNadarayaWatson/predict_in_parallel(not_correct_input)", {
   instance <- GeneralisedNadarayaWatson$new()
   instance$run_cluster(1)
-  expect_error(instance$predict_in_parallel(rep(NaN), 1), "The model was not trained correctly")
+  expect_error(instance$predict_in_parallel(rep(NaN), cbind(NaN), 1), 
+               "The model was not trained correctly")
   instance$stop_cluster()
-  
+
   instance <- GeneralisedNadarayaWatson$new()
   instance$X_train <- 1:2
   instance$Y_train <- 1:2
   instance$A <- matrix(NaN, 2, 2)
   instance$run_cluster(1)
-  expect_error(instance$predict(rep(NaN, 1), 1), "The model coefficients are not numbers")
+  expect_error(instance$predict_in_parallel(rep(NaN, 1), cbind(NaN, 1), 1), 
+               "The model coefficients are not numbers")
+  instance$stop_cluster()
+
+  instance$run_cluster()
+  expect_error(instance$predict_in_parallel(rep(NaN, max_threads), matrix(NaN, max_threads, 2), h = 1), 
+               "The model coefficients are not numbers")
   instance$stop_cluster()
   
+  instance$A <- matrix(1, 2, 2)
   instance$run_cluster()
-  expect_error(instance$predict(rep(NaN, max_threads), 1), "The model coefficients are not numbers")
+  expect_error(instance$predict_in_parallel(rep(NaN, 1), matrix(NaN, 2, 2), 1), 
+               "Not correct input dimensions")
   instance$stop_cluster()
 })
 
@@ -144,13 +168,22 @@ test_that("Test GeneralisedNadarayaWatson/predict_in_parallel(correct_input)", {
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(X_train, Y_train, w_coeff)
   instance$run_cluster(2)
-  expect_equal(instance$predict_in_parallel(X_test, 1), rbind(c(2, 2), c(2, 2)))
-  expect_equal(instance$predict_in_parallel(X_test, .5), rbind(c(2, 2), c(2, 2)))
+  expect_equal(instance$predict_in_parallel(X_test, w_coeff, 1),
+               list("prediction" = rbind(c(2, 2), c(2, 2)),
+                    "A_test" = cbind(c(0, 2), c(4, -2))))
+  expect_equal(instance$predict_in_parallel(X_test, w_coeff, .5),
+               list("prediction" = rbind(c(2, 2), c(2, 2)),
+                    "A_test" = cbind(c(0, 2), c(4, -2))))
   instance$stop_cluster()
+  
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(c(X_train, 3), c(Y_train, 1), w_coeff2)
   instance$run_cluster(2)
-  expect_equal(instance$predict_in_parallel(X_test2, 1), rbind(c(2, 2, 2), c(2, 2, 2)))
+  expect_equal(instance$predict_in_parallel(c(X_test2, 1), w_coeff2, 1), 
+               list("prediction" = matrix(2, 3, 3), 
+                    "A_test" = cbind(c(3.90625, -0.37500, -0.53125),
+                                     c(-0.15625, 3.37500, -0.21875), 
+                                     c(-1.09375, -0.37500, 4.46875))))
   instance$stop_cluster()
 })
 
@@ -167,13 +200,17 @@ test_that("Test output type GeneralisedNadarayaWatson/predict_in_parallel", {
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(X_train, Y_train, w_coeff)
   instance$run_cluster(2)
-  expect_true(is.matrix(instance$predict_in_parallel(X_test, 1)))
-  expect_true(is.matrix(instance$predict_in_parallel(X_test, .5)))
+  expect_true(is.list(instance$predict_in_parallel(X_test, w_coeff, 1)))
+  expect_true(is.matrix(instance$predict_in_parallel(X_test, w_coeff, 1)[[1]]))
+  expect_true(is.matrix(instance$predict_in_parallel(X_test, w_coeff, .5)[[2]]))
   instance$stop_cluster()
+
   instance <- GeneralisedNadarayaWatson$new()
   instance$train(c(X_train, 3), c(Y_train, 1), w_coeff2)
   instance$run_cluster(2)
-  expect_true(is.matrix(instance$predict_in_parallel(X_test2, 1)))
+  expect_true(is.list(instance$predict_in_parallel(c(X_test2, 1), w_coeff2, 1)))
+  expect_true(is.matrix(instance$predict_in_parallel(c(X_test2, 1), w_coeff2, 1)[[1]]))
+  expect_true(is.matrix(instance$predict_in_parallel(c(X_test2, 1), w_coeff2, 1)[[2]]))
   instance$stop_cluster()
 })
 
--- tools/GeneralizedNadarayaWatson.R
+++ tools/GeneralizedNadarayaWatson.R
@@ -21,53 +21,68 @@ GeneralisedNadarayaWatson <- R6Class("GeneralisedNadarayaWatson",
                                          self$A <- NULL
                                          self$max_threads <- max_threads
                                        },
-                                       train = function(X_train, Y_train, w_coeff){
+                                       train = function(X_train, Y_train, W_train){
                                          if (!is.vector(X_train, mode = "numeric") | 
                                              !is.vector(Y_train, mode = "numeric") | 
-                                             !is.matrix(w_coeff)) 
-                                         {stop("Not correct class atributes")}
+                                             !is.matrix(W_train))
+                                           {stop("Not correct class attributes")}
                                          
-                                         if (any(c(length(X_train), length(Y_train), nrow(w_coeff)) != 
+                                         if (any(c(length(X_train), length(Y_train), nrow(W_train)) != 
                                                  rep(length(X_train), 3)))
-                                         {stop("Not correct input dimensions")}
+                                           {stop("Not correct input dimensions")}
                                          self$X_train <- X_train
                                          self$Y_train <- Y_train
-                                         self$A <- acoeff(w_coeff)
+                                         self$A <- acoeff(W_train)
                                        }, 
-                                       predict = function(X_test, h){
-                                         if (is.numeric(X_test) != TRUE | is.numeric(h) != TRUE)
-                                         {stop("Not correct class atributes")}
+                                       predict = function(X_test, W_test, h){
+                                         if (is.numeric(X_test) != TRUE | 
+                                             is.numeric(h) != TRUE | 
+                                             is.matrix(W_test) != TRUE)
+                                           {stop("Not correct class attributes")}
                                          if (is.null(self$X_train) | 
                                              is.null(self$Y_train) |
                                              is.null(self$A))
-                                         {stop("The model was not trained correctly")}
-                                         if (!is.null(self$A)) 
-                                         {if (any(is.na(self$A))) {stop("The model coefficients are not numbers")}}
+                                           {stop("The model was not trained correctly")}
+                                         if (!is.null(self$A))
+                                           {if (any(is.na(self$A))) {stop("The model coefficients are not numbers")}}
+                                         if (length(X_test) != nrow(W_test))
+                                           {stop("Not correct input dimensions")}
                                          results <- sapply(X_test, 
                                                            function(x, h){
                                                              res <- nw_any_components(x, self$X_train, self$Y_train, h, self$A)
                                                              return(res)
                                                            }, h = h)
-                                         return(t(results))
+                                         return(list("prediction" = t(results), 
+                                                     "A_test" = tryCatch(acoeff(W_test),
+                                                                         error = function(e){
+                                                                           return(paste0("acoeff caused the error while predict: '", e, "'"))
+                                                                           })))
                                        },
-                                       predict_in_parallel = function(X_test, h){
-                                         if (is.numeric(X_test) != TRUE | is.numeric(h) != TRUE)
-                                         {stop("Not correct class atributes")}
+                                       predict_in_parallel = function(X_test, W_test, h){
+                                         if (is.numeric(X_test) != TRUE | 
+                                             is.numeric(h) != TRUE |
+                                             is.matrix(W_test) != TRUE)
+                                           {stop("Not correct class attributes")}
                                          if (is.null(self$X_train) | 
                                              is.null(self$Y_train) |
                                              is.null(self$A))
-                                         {stop("The model was not trained correctly")}
-                                         if (!is.null(self$A)) 
-                                         {if (any(is.na(self$A))) {stop("The model coefficients are not numbers")}}
+                                           {stop("The model was not trained correctly")}
+                                         if (!is.null(self$A))
+                                           {if (any(is.na(self$A))) {stop("The model coefficients are not numbers")}}
+                                         if (length(X_test) != nrow(W_test))
+                                           {stop("Not correct input dimensions")}
                                          n_rows <- length(X_test)
                                          list_of_parts <- split_k_parts(k = self$max_threads, nrows = n_rows)
-                                         res <- foreach(each_part = list_of_parts, 
-                                                        .combine = list, 
+                                         res <- foreach(each_part = list_of_parts,
+                                                        .combine = list,
                                                         .multicombine = TRUE,
-                                                        .export = "self") %dopar% {
-                                                          self$predict(X_test[as.vector(each_part)], h)
+                                                        .export = c("self", "W_test", "h")) %dopar% {
+                                                          pr <- self$predict(X_test[each_part],
+                                                                             matrix(W_test[each_part, ], nrow = length(each_part)), h)
+                                                          pr$prediction
                                                           }
-                                          return(do.call(rbind, res))
+                                          return(list("prediction" = do.call(rbind, res), 
+                                                      "A_test" = acoeff(W_test)))
                                         },
                                        stop_cluster = function(){
                                          stopImplicitCluster()
