5a1464326f01c1b6d8ddea177c0c846a45b2610f
galinadychko <galiadychko@outlook.com>
2019-02-18 18:44:55 +0200

    fix: Add computence generalized Nadaraya-Watson estimator with different smoothing parameters for different components

--- tests/test_NadarayaWatson.R
+++ tests/test_NadarayaWatson.R
@@ -3,7 +3,8 @@ library(testthat)
 
 test_that("Test Epanechnikov(not_correct_input_type)",{
   M <- matrix(NaN, nrow = 2, ncol = 2)
-  expect_error(Epanechnikov(M), "Not correct input type")
+  expect_error(Epanechnikov(as.data.frame(M)), "Not correct input type")
+  expect_error(Epanechnikov(as.list(M)), "Not correct input type")
 })
 
 
@@ -34,7 +35,7 @@ test_that("Test nw_any_components(not_correct_input_type)",{
   expect_error(nw_any_components(as.numeric(x), X_train, Y_train, 1, as.numeric(A)), "Not correct input type")
   expect_error(nw_any_components(as.numeric(x), X_train, Y_train, 1, A), "Not correct input type")
   expect_error(nw_any_components(NaN, X_train, Y_train, 1, A), "Not correct input type")
-  expect_error(nw_any_components(2, rep(2, 2), rep(2, 2), 1, rep(0, 2)), "Devision by zero")
+  expect_error(nw_any_components(2, rep(2, 2), rep(2, 2), 1, matrix(0, 2, 2)), "Devision by zero")
 })
 
 
@@ -46,6 +47,7 @@ test_that("Test nw_any_components(different_values)",{
   A <- rep(1, 2)
   expect_equal(nw_any_components(x, X_train, Y_train, h, A), 2)
   expect_equal(nw_any_components(x, X_train, Y_train, h, matrix(1:4, 2, 2)), c(2, 2))
+  expect_equal(nw_any_components(x, X_train, Y_train, c(1, 2), matrix(1:4, 2, 2)), c(2, 2))
 })
 
 
@@ -56,5 +58,5 @@ test_that("Test output type nw_any_components",{
   h <- 1
   A <- rep(1, 2)
   expect_true(is.vector(nw_any_components(x, X_train, Y_train, h, A)))
-  expect_true(is.vector(nw_any_components(x, X_train, Y_train, h, matrix(1:4, 2, 2))))
+  expect_true(is.vector(nw_any_components(x, X_train, Y_train, c(1, .9), matrix(1:4, 2, 2))))
 })
--- tools/NadarayaWatson.R
+++ tools/NadarayaWatson.R
@@ -4,7 +4,8 @@
 
 
 Epanechnikov <- function(x){
-  if (is.matrix(x) == TRUE) {stop("Not correct input type")}
+  if (is.matrix(x) != TRUE & is.vector(x,  mode = "numeric") != TRUE)
+    {stop("Not correct input type")}
   result <- 3/4*(1 - x^2)*(abs(x) <= 1)
   return(result)
 }
@@ -15,15 +16,18 @@ Epanechnikov <- function(x){
 # --------------------------------------------------------
 
 nw_any_components <- function(x, X_train, Y_train, h, A){
-  if (is.numeric(X_train) != TRUE | is.numeric(Y_train) != TRUE | 
-      is.numeric(x) != TRUE) {stop("Not correct input type")}
-  if (is.numeric(A) == TRUE) {A <- as.matrix(A, ncol = 1)}
+  if (is.vector(X_train, mode = "numeric") != TRUE | 
+      is.vector(Y_train, mode = "numeric") != TRUE | 
+      is.vector(x, mode = "numeric") != TRUE) 
+    {stop("Not correct input type")}
+  if (is.vector(A, mode = "numeric") == TRUE) {A <- as.matrix(A, ncol = 1)}
   if (length(x) > 1) {stop("Not correct input type")}
+  
   n_colms <- ncol(A)
-  numerator <- colSums(matrix(A * Y_train * Epanechnikov((x - X_train)/h), ncol = n_colms))
-  denominator <- colSums(matrix(A * Epanechnikov((x - X_train)/h), ncol = n_colms))
-  
+  n_rows <- nrow(A)
+  h_matrix <- 1/matrix(t(h), nrow = n_rows, ncol = n_colms, byrow = TRUE)
+  denominator <- colSums(matrix(A * Epanechnikov(h_matrix*(x - X_train)), ncol = n_colms))
   if (sum(denominator) == 0) {stop("Devision by zero")}
-  
+  numerator <- colSums(matrix(A * Y_train * Epanechnikov(h_matrix*(x - X_train)), ncol = n_colms))
   return(numerator/denominator)
 }
