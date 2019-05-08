3c82666886bf4b88de70352ba8b353f561949baf
galinadychko <galiadychko@outlook.com>
2019-05-08 11:52:51 +0300

    fix: Change the way of splitlin data into k parts

--- tools/CommonTools.R
+++ tools/CommonTools.R
@@ -1,11 +1,14 @@
 "%&%" <- function(x, y){paste(x, y, sep = "")}
 
-
-split_k_parts <- function(k, nrows, random_seed){
+split_k_parts <- function(k, nrows){
   if (nrows < k) {stop("Number of observations is smaller than number of folds")}
-  set.seed(random_seed)
-  sample_size <- ceiling(nrows/k)
-  id_list <- split(1:(k*sample_size), rep(1:k, each = sample_size))
-  id_list[[k]] <- id_list[[k]][id_list[[k]] <= nrows]
-  return(id_list)
+  a <- 1:nrows
+  kk <- nrows %/% k
+  m <- nrows %% k
+  l <- list()
+  for(i in 0:(k-1)){
+    l[[i+1]] <- a[(i * kk + min(i, m) + 1):((i+1) * kk + min(i + 1, m))]
+  }
+  return(l)
 }
+split_k_parts(k = 11, nrows = 100)
